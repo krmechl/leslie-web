bf32a90c268b653a072be32e93bbe3308c96c2b8
kr mechl <krmechl@gmail.com>
2013-07-12 11:45:38 -0400

    7-12-2013

--- files/views/assets/js/services/service.js
+++
@@ -1,9 +0,0 @@
-/*
- * Services
- */
-
-var getWeather = new Appery.RestService({
-    'url': 'http://api.worldweatheronline.com/free/v1/weather.ashx',
-    'dataType': 'jsonp',
-    'type': 'get',
-});
